2fee9939eea17e85a72cd22b7493bd5333abad91
Kaan Kani İnce <kkince@newmind.ai>
2024-07-05 11:12:18 +0300

    Accordion created

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Accordian from "./compenents/accordion";
+
+function App() {
+  return (
+    <div className="App">
+      {/* Accordian component*/}
+      <Accordian/>
+    </div>
+  );
+}
+
+export default App;
